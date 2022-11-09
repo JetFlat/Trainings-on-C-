4ef3e2c8aa92006acb7fb4e834e40d41ebb0c08d
Oleksandr <113677456+JetFlat@users.noreply.github.com>
2022-11-09 17:06:27 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Index
+{
+    class Program
+    {
+        static int[]Massive(int[] array, int index)
+        {
+            int i = 0;
+            int[] newarray = new int[array.Length - 1];
+            List<int> list = new List<int>(newarray);
+            
+            if (index > array.Length)
+                return array;
+            while (i < index)
+            {
+                list.Add(array[i++]);
+                i++;
+            }
+            while (i < newarray.Length)
+            {
+                newarray[i] = array[i + 1];
+                i++;
+            }            
+            
+            
+            
+            return newarray;
+        }static void Main()
+        {
+            int[] test = new int[] { 1, 2, 3, 4, 5, 6 };
+            Array.ForEach(test, Console.WriteLine);
+            int[] result = Massive(test, 2);
+            Array.ForEach(result, Console.WriteLine);
+        }
+    }
+}
