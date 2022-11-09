b3b4c67a1a3f7d46f276cb54926d9846a61207af
Oleksandr <113677456+JetFlat@users.noreply.github.com>
2022-11-09 14:09:22 +0100

    Update and rename Program.cs to Coins.cs

--- Coins.cs
+++ Coins.cs
@@ -1,4 +1,7 @@
-﻿using System;
+// Create a function which will count the minimum number of coins, which saller should give as a change. He can give only 25, 10, 5 and 1 (nominal of coins).
+// For example: Change is 24 - He will give 2 x 10 + 4 x 1 coins, totaly = 6.
+
+using System;
 using System.Reflection.Metadata.Ecma335;
 
 namespace Coins
@@ -7,7 +10,7 @@ namespace Coins
     {
         public static int NumCoins (int change)
         {
-            int i = 0;
+            int i = 0; // i will count a number of coins to be given.
             while (change != 0)
             {
                 if (change >= 25) 
@@ -41,7 +44,7 @@ namespace Coins
         }static void Main()
         {
             
-            int i = Convert.ToInt16(Console.ReadLine());
+            int i = Convert.ToInt16(Console.ReadLine());// for user to put his own value to be considered as a change.
             while (i < 0)
             {
                 Console.WriteLine("Change can`t be negative");
