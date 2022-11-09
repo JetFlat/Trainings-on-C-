cbc5d073a312807b605406654256fe08eb372edb
Oleksandr <113677456+JetFlat@users.noreply.github.com>
2022-11-09 17:07:38 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("Please, enter your role");
+            string role = Console.ReadLine();
+
+            if (role == "Admin")
+            {
+                Console.WriteLine("Please enter Admin password to get into Admin settings");
+                string password = Console.ReadLine();
+
+                do
+                {
+                    Console.WriteLine("Please, try again");
+                }
+                while (password.Length < 8);
+
+            }
+
+            else
+            {
+                Console.WriteLine("You entered as a Guest ");
+            }
+        }   
+    }
+}
+
+
