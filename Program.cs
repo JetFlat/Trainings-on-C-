0900c400e381626332f87bbc278da4691c1e6487
Oleksandr <113677456+JetFlat@users.noreply.github.com>
2022-11-09 17:10:21 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+
+namespace NewProject
+{
+    class Program
+    {
+        public static string Write (string s1, string s2)
+        {
+            
+            int sum = int.Parse(s1) + int.Parse(s2);
+            return sum.ToString();
+            
+        }
+        static void Main()
+        {
+            string s1 = "10";
+            string s2 = "13";
+            string result = Write (s1, s2);
+            Console.WriteLine(result);
+
+        }
+    }
+}
